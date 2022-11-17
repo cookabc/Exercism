53ab7d2ee0ece3fd7be7853bee3b00d1aae473b9
xugang <chuangcius@gmail.com>
2022-11-17 11:27:55 +0800

    kotlin: add "Flatten Array"

---
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -0,0 +1,10 @@
+object Flattener {
+    fun flatten(source: Collection<Any?>): List<Any> {
+        return source.filterNotNull().flatMap {
+            when (it) {
+                is Collection<*> -> flatten(it)
+                else -> listOf(it)
+            }
+        }
+    }
+}
