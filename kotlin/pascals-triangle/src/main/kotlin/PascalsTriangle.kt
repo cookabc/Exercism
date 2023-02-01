f153d0fba12c6c405dd273e91f75b6061269b5f7
chuangcius <chuangcius@gmail.com>
2023-02-01 10:10:58 +0800

    add: "Pascal's Triangle"

---
+++ kotlin/pascals-triangle/src/main/kotlin/PascalsTriangle.kt
@@ -0,0 +1,8 @@
+object PascalsTriangle {
+
+    fun computeTriangle(rows: Int): List<List<Int>> {
+        return generateSequence(listOf(1)) { prev ->
+            listOf(1) + prev.windowed(2).map { it.sum() } + listOf(1)
+        }.take(rows).toList()
+    }
+}
