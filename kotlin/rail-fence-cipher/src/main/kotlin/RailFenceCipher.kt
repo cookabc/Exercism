63974e20b9943685709397427cd6f26a4ebad32d
chuangcius <chuangcius@gmail.com>
2023-02-20 08:56:02 +0800

    add: "Rail Fence Cipher"

---
+++ kotlin/rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
@@ -0,0 +1,30 @@
+class RailFenceCipher(private val rows: Int) {
+
+    fun getEncryptedData(plainText: String): String {
+        return plainText.asSequence().zip(tracks.take(plainText.length))
+            .sortedBy { it.second }
+            .map { it.first }
+            .joinToString("")
+    }
+
+    fun getDecryptedData(cipherText: String): String =
+        tracks.take(cipherText.length)
+            .mapIndexed { index, track -> index to track }
+            .toList()
+            .sortedBy { it.second }
+            .map { it.first }
+            .zip(cipherText.asIterable())
+            .sortedBy { it.first }
+            .map { it.second }
+            .joinToString("")
+
+    private val tracks = sequence {
+        var current = 0
+        var up = true
+        while (true) {
+            yield(current)
+            current = if (up) current + 1 else current - 1
+            up = if (up) current < rows - 1 else current == 0
+        }
+    }
+}
