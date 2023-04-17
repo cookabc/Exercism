1f82adf4b81cae120d378a5eb547ff3299a1f2ff
chuangcius <chuangcius@gmail.com>
2023-04-17 18:04:56 +0800

    update: Party Robot

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -1,26 +1,20 @@
 package partyrobot
 
-import "strconv"
-import "strings"
+import "fmt"
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	// panic("Please implement the Welcome function")
-	return "Welcome to my party, " + name + "!"
+	return fmt.Sprintf("Welcome to my party, %s!", name)
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	// panic("Please implement the HappyBirthday function")
-	return "Happy birthday " + name + "! You are now " + strconv.Itoa(age) + " years old!"
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	// panic("Please implement the AssignTable function")
-	tableNumberString := strconv.Itoa(table)
-	if len(tableNumberString) < 3 {
-		tableNumberString = strings.Repeat("0", 3-len(tableNumberString)) + tableNumberString
-	}
-	return "Welcome to my party, " + name + "!\nYou have been assigned to table " + tableNumberString + ". Your table is " + direction + ", exactly " + strconv.FormatFloat(distance, 'f', 1, 64) + " meters from here.\nYou will be sitting next to " + neighbor + "."
+	return fmt.Sprintf(`Welcome to my party, %s!
+You have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.
+You will be sitting next to %s.`, name, table, direction, distance, neighbor)
 }
