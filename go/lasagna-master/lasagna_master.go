16c855b1a5303062f01671f59425406b822b8a3a
chuangcius <chuangcius@gmail.com>
2023-04-18 18:07:37 +0800

    update: Lasagna Master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,47 +1,45 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
-func PreparationTime(layers []string, time int) int {
-	if time == 0 {
-		time = 2
+const (
+	defautlPreparationTimePerLayer = 2
+	noodleQtyPerLayer              = 50
+	sauceQtyPerLayer               = 0.2
+	defaultServingInRecipe         = 2
+)
+
+// PreparationTime estimates the total preparation time based on the number of layers
+func PreparationTime(layers []string, preparationTimePerLayer int) int {
+	if preparationTimePerLayer <= 0 {
+		preparationTimePerLayer = defautlPreparationTimePerLayer
 	}
-	return len(layers) * time
+	return len(layers) * preparationTimePerLayer
 }
 
-// TODO: define the 'Quantities()' function
+// Quantities determines the quantity of noodles and sauce needed to make your meal
 func Quantities(layers []string) (int, float64) {
-	noodles := 0
-	sauce := 0.0
-	for _, layer := range layers {
-		if layer == "noodles" {
-			noodles += 50
-		} else if layer == "sauce" {
-			sauce += 0.2
+	noodleLayers := 0
+	sauceLayers := 0
+	for _, l := range layers {
+		switch l {
+		case "noodles":
+			noodleLayers++
+		case "sauce":
+			sauceLayers++
 		}
 	}
-	return noodles, sauce
+	return noodleLayers * noodleQtyPerLayer, float64(sauceLayers) * sauceQtyPerLayer
 }
 
-// TODO: define the 'AddSecretIngredient()' function
-func AddSecretIngredient(friendsList []string, myList []string) {
-	lastIngredient := friendsList[len(friendsList)-1]
-	myList[len(myList)-1] = lastIngredient
+// AddSecretIngredient adds the secret ingredient to your recipe
+func AddSecretIngredient(friendsList, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
 }
 
-// TODO: define the 'ScaleRecipe()' function
-func ScaleRecipe(recipe []float64, scale int) []float64 {
-	scaledRecipe := make([]float64, len(recipe))
-	for i, amount := range recipe {
-		scaledRecipe[i] = amount * float64(scale) / 2
+// ScaleRecipe gives the amounts needed for the desired number of portions
+func ScaleRecipe(quantities []float64, amount int) []float64 {
+	var scaledAmounts []float64
+	for _, recipeQty := range quantities {
+		scaledAmounts = append(scaledAmounts, recipeQty/defaultServingInRecipe*float64(amount))
 	}
-	return scaledRecipe
+	return scaledAmounts
 }
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
