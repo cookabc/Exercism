5c3d2690d946af6b0c9bb41762742c7259733a89
chuangcius <chuangcius@gmail.com>
2023-04-21 13:12:56 +0800

    update: The Farm

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -23,19 +23,18 @@ func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
 				return 0, &SillyNephewError{"negative fodder"}
 			}
 			if amount >= 0 {
-				amount *= 2
+				return amountPerCow(amount*2, cows), nil
 			}
-			return amountPerCow(amount, cows), nil
 		}
 		return 0, &SillyNephewError{"non-scale error"}
 	}
 	if amount < 0 {
 		return 0, &SillyNephewError{"negative fodder"}
 	}
-	if cows == 0 {
-		return 0, &SillyNephewError{"division by zero"}
-	}
-	if cows < 0 {
+	if cows <= 0 {
+		if cows == 0 {
+			return 0, &SillyNephewError{"division by zero"}
+		}
 		return 0, &SillyNephewError{fmt.Sprintf("silly nephew, there cannot be %d cows", cows)}
 	}
 	return amountPerCow(amount, cows), nil
